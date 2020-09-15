7ef03a9d47def88de0a10318a765d0f10c3c149c
Dominic Faulring <domcodesjs@gmail.com>
2020-09-14 20:27:08 -0700

    done with basic reqs

--- js/main.js
+++ js/main.js
@@ -1,12 +1,33 @@
-class ListItem {
-  constructor(id, text) {
-    this.id = id;
-    this.text = text;
-  }
+// attaches to existing list items when the page loads
+addDeleteItemEventListener();
+addToggleItemListener();
+
+$('#js-shopping-list-form').submit(function (e) {
+  e.preventDefault();
+
+  // we create a new item
+  $('ul').append(
+    `<li> <span class="shopping-item"> ${$(
+      '#shopping-list-entry'
+    ).val()} </span> <div class="shopping-item-controls"><button class="shopping-item-toggle"><span class="button-label">check</span></button><button class="shopping-item-delete"><span class="button-label">delete</span></button></div> </li>`
+  );
+
+  // attaches to new list items that are created
+  addDeleteItemEventListener();
+  addToggleItemListener();
+});
+
+function addToggleItemListener() {
+  $('.shopping-item-toggle').on('click', function () {
+    $(this)
+      .closest('li')
+      .find('.shopping-item')
+      .toggleClass('shopping-item__checked');
+  });
 }
 
-class List {
-  constructor() {
-    this.store = {};
-  }
+function addDeleteItemEventListener() {
+  $('.shopping-item-delete').on('click', function () {
+    this.closest('li').remove();
+  });
 }
